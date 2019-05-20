744dc77a220fd938312596582e3873ce9e22554f
Derek Rushforth <derekrushforth@gmail.com>
2019-05-20 10:40:36 -0700

    Run through code formatter

--- test/integration/email.test.ts
+++ test/integration/email.test.ts
@@ -1,24 +1,29 @@
-import {expect} from "chai";
-import "mocha";
+import { expect } from 'chai'
+import 'mocha'
 import * as execa from 'execa'
-import * as nconf from "nconf";
+import * as nconf from 'nconf'
 
-const testingKeys = nconf.env().file({file: __dirname + "/../config/testing_keys.json"});
+const testingKeys = nconf
+  .env()
+  .file({ file: __dirname + '/../config/testing_keys.json' })
 
-describe("Email send command", () => {
-    const serverToken: string = testingKeys.get("SERVER_TOKEN");
-    const options: execa.CommonOptions = {env: {'POSTMARK_SERVER_TOKEN': serverToken}};
-    const CLICommand: string = './dist/index.js';
-    const commandParameters = ['email', 'raw'];
+describe('Email send command', () => {
+  const serverToken: string = testingKeys.get('SERVER_TOKEN')
+  const options: execa.CommonOptions = {
+    env: { POSTMARK_SERVER_TOKEN: serverToken },
+  }
+  const CLICommand: string = './dist/index.js'
+  const commandParameters = ['email', 'raw']
 
-    describe("Not enough arguments", () => {
-        it('no arguments', async () => {
-            try {
-                await execa(CLICommand, [], options);
-            } catch (error) {
-                expect(error.message).to.include('Not enough non-option arguments: got 0, need at least 1');
-            }
-        });
-    });
-});
-
+  describe('Not enough arguments', () => {
+    it('no arguments', async () => {
+      try {
+        await execa(CLICommand, [], options)
+      } catch (error) {
+        expect(error.message).to.include(
+          'Not enough non-option arguments: got 0, need at least 1'
+        )
+      }
+    })
+  })
+})
--- test/integration/servers.test.ts
+++ test/integration/servers.test.ts
@@ -1,27 +1,36 @@
-import { expect } from "chai";
-import "mocha";
+import { expect } from 'chai'
+import 'mocha'
 import * as execa from 'execa'
-import * as nconf from "nconf";
+import * as nconf from 'nconf'
 
-const testingKeys = nconf.env().file({ file: __dirname + "/../config/testing_keys.json" });
+const testingKeys = nconf
+  .env()
+  .file({ file: __dirname + '/../config/testing_keys.json' })
 
-describe("Servers list command", () => {
-    const accountToken: string = testingKeys.get("ACCOUNT_TOKEN");
-    const options: execa.CommonOptions = { env: {'POSTMARK_ACCOUNT_TOKEN': accountToken} };
-    const CLICommand: string = './dist/index.js';
-    const commandParameters: string[] = ['servers', 'list'];
+describe('Servers list command', () => {
+  const accountToken: string = testingKeys.get('ACCOUNT_TOKEN')
+  const options: execa.CommonOptions = {
+    env: { POSTMARK_ACCOUNT_TOKEN: accountToken },
+  }
+  const CLICommand: string = './dist/index.js'
+  const commandParameters: string[] = ['servers', 'list']
 
-    it('list', async () => {
-        const {stdout} = await execa(CLICommand, commandParameters, options);
-        const servers = JSON.parse(stdout);
-        expect(servers.TotalCount).to.be.gte(0);
-    });
+  it('list', async () => {
+    const { stdout } = await execa(CLICommand, commandParameters, options)
+    const servers = JSON.parse(stdout)
+    expect(servers.TotalCount).to.be.gte(0)
+  })
 
-    it('list - invalid token', async () => {
-        const invalidOptions: execa.CommonOptions = { env: {'POSTMARK_ACCOUNT_TOKEN': 'test'} };
+  it('list - invalid token', async () => {
+    const invalidOptions: execa.CommonOptions = {
+      env: { POSTMARK_ACCOUNT_TOKEN: 'test' },
+    }
 
-        const {stderr} = await execa(CLICommand, commandParameters, invalidOptions);
-        expect(stderr).to.include('InvalidAPIKeyError');
-    });
-});
-
+    const { stderr } = await execa(
+      CLICommand,
+      commandParameters,
+      invalidOptions
+    )
+    expect(stderr).to.include('InvalidAPIKeyError')
+  })
+})
