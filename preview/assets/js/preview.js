096bd61e572daeb896d92fb1bbaa8a803e0b3682
Derek Rushforth <derekrushforth@gmail.com>
2019-11-20 14:24:55 -0700

    Device viewport toggle

--- preview/assets/js/preview.js
+++ preview/assets/js/preview.js
@@ -1,26 +1,82 @@
-var html = document.querySelector('.js-html')
-var toggles = document.querySelectorAll('.js-toggle-mode')
-var activeToggleClass = 'toggle_item--active'
+var activeToggleClass = 'is-active'
 
-toggles.forEach(function(toggle) {
+/**
+ * Mode toggles
+ */
+var mode = document.querySelectorAll('.js-mode')
+var currentMode = 'html'
+
+mode.forEach(function(toggle) {
   toggle.addEventListener('click', function(event) {
     event.preventDefault()
-    // Reset state
-    resetMode()
+
+    // Reset all classes
+    mode.forEach(function(modeToggle) {
+      // Remove active class from all toggles
+      modeToggle.classList.remove(activeToggleClass)
+
+      // Hide all views
+      document
+        .querySelector('.js-' + modeToggle.dataset.mode)
+        .classList.add('hidden')
+    })
 
     // Add active class
+    currentMode = this.dataset.mode
     this.classList.add(activeToggleClass)
 
-    // Show preview
+    // Show view
     document
       .querySelector('.js-' + this.dataset.mode)
       .classList.remove('hidden')
   })
 })
 
-function resetMode() {
-  toggles.forEach(function(toggle) {
-    toggle.classList.remove(activeToggleClass)
-    document.querySelector('.js-' + toggle.dataset.mode).classList.add('hidden')
+/**
+ * View toggles
+ */
+var view = document.querySelectorAll('.js-view')
+var currentView = 'desktop'
+
+view.forEach(function(toggle) {
+  // Add click event
+  toggle.addEventListener('click', function(event) {
+    event.preventDefault()
+
+    view.forEach(function(viewToggle) {
+      // Remove active class from all toggles
+      viewToggle.classList.remove(activeToggleClass)
+
+      document.querySelectorAll('.preview-iframe').forEach(function(item) {
+        item.classList.remove('preview-iframe--mobile')
+      })
+    })
+
+    currentView = this.dataset.view
+    this.classList.add(activeToggleClass)
+
+    if (this.dataset.view === 'mobile') {
+      document.querySelectorAll('.preview-iframe').forEach(function(item) {
+        item.classList.add('preview-iframe--mobile')
+      })
+    }
   })
+})
+
+function keypress(e) {
+  var evt = window.event ? event : e
+
+  // ctrl + v
+  if (evt.keyCode == 86 && evt.ctrlKey) {
+    var view = currentView === 'desktop' ? 'mobile' : 'desktop'
+    document.querySelector('.js-view[data-view="' + view + '"]').click()
+  }
+
+  // ctrl + m
+  if (evt.keyCode == 77 && evt.ctrlKey) {
+    var mode = currentMode === 'html' ? 'text' : 'html'
+    document.querySelector('.js-mode[data-mode="' + mode + '"]').click()
+  }
 }
+
+document.onkeydown = keypress
